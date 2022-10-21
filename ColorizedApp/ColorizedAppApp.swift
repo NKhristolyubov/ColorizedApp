f2eafe7aff5fae1df40402e43c7c332df6e8cd37
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-10-21 14:18:56 +0400

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Николай Христолюбов on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
